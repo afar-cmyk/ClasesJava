9f8fa51cb85b87ea0ff3e455d844a717e305b48b
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 06:17:59 -0500

    Ejercicio_23 cambios al enunciado

--- Ejercicios_U01/src/Ejercicio_23.java
+++ Ejercicios_U01/src/Ejercicio_23.java
@@ -4,7 +4,8 @@ public class Ejercicio_23 {
     //Solo se puede utilizar la función main() solo para hacer el llamado a las funciones que se quieren probar.
        
     /*
-    23. Programa que lea un carácter por teclado y compruebe si es una letra mayúscula.
+    23. Programa que lea dos números por teclado y muestre el resultado de la división del primer número 
+    por el segundo. Se debe comprobar que el divisor no puede ser cero.
     */
 
     public void name() {
