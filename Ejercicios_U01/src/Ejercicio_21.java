d6587eabc7aab46408a5e127db76eeb879afd714
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 03:36:44 -0500

    Ejercicio_21 Updated

--- Ejercicios_U01/src/Ejercicio_21.java
+++ Ejercicios_U01/src/Ejercicio_21.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Ejercicio_21 {
 
@@ -7,12 +8,25 @@ public class Ejercicio_21 {
     21. Programa que lea un número entero y muestre si el número es múltiplo de 10.
     */
 
-    public void name() {
+    public static final Scanner ENTRADA() {
+        // Objeto Scanner llamado Entrada.
+        Scanner entrada = new Scanner (System.in);
+        return entrada;
+    }
+
+    public static void calculos() {
+        // Informa el funcionamiento del programa.
+        System.out.println("\n--- Programa que determina si el numero ingresado es multiplo de 10. ---");
         
+        // Solicita un numero entero por consola.
+        System.out.println("\nIngrese un numero entero: ");
+        int numero = ENTRADA().nextInt();
+
+        // Operador condicional ternario que indica si es multiplo de 10 o no.
+        System.out.println("\nEl número " + numero + (numero % 10 == 0 ? " Sí es" : " No es") + " multiplo de 10.");
     }
 
     public static void main(String[] args) {
-        
+        calculos();
     }
-
 }
