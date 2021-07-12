1dd08ecce6712b05221bbc794549e95bf0f16460
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 06:34:38 -0500

    Ejercicio_23 Updated

--- Ejercicios_U01/src/Ejercicio_23.java
+++ Ejercicios_U01/src/Ejercicio_23.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Ejercicio_23 {
 
@@ -8,12 +9,33 @@ public class Ejercicio_23 {
     por el segundo. Se debe comprobar que el divisor no puede ser cero.
     */
 
-    public void name() {
-        
+    public static final Scanner ENTRADA() {
+        // Objeto Scanner llamado Entrada.
+        Scanner entrada = new Scanner(System.in);
+        return entrada;
+    }
+
+    public static void dividirDosNumeros() {
+        // Mensaje inicial.
+        System.out.println("--- Programa que dos números ---");
+
+        // Solicita dos números al usuario.
+        System.out.print("\nIngrese el primero numero: ");
+        Double numeroUno = ENTRADA().nextDouble();
+
+        System.out.print("Ingrese el segundo numero: ");
+        Double numeroDos = ENTRADA().nextDouble();
+
+        Double division = numeroUno / numeroDos;
+
+        if(numeroUno == 0 | numeroDos == 0){
+            System.out.printf("\n---> ¡Error!, No se puede dividir sobre cero.");
+        }else{
+            System.out.printf("\nLa división entre %1$s y %2$s es: %3$s.",numeroUno,numeroDos,division); 
+        }
     }
 
     public static void main(String[] args) {
-        
+        dividirDosNumeros();
     }
-
 }
