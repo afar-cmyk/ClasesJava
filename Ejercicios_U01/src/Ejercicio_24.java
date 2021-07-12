710ae66b0df8429c7c52d3835040cb2b1eebc663
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 07:22:33 -0500

    Ejercicio_24 Updated

--- Ejercicios_U01/src/Ejercicio_24.java
+++ Ejercicios_U01/src/Ejercicio_24.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Ejercicio_24 {
 
@@ -7,12 +8,37 @@ public class Ejercicio_24 {
     24. El programa lee por teclado tres números enteros y calcula y muestra el mayor de los tres.
     */
 
-    public void name() {
+    public static final Scanner ENTRADA() {
+        // Objeto Scanner llamado Entrada.
+        Scanner entrada = new Scanner(System.in);
+        return entrada;
+    }
+
+    public static void numeroMayor() {
+        // Mensaje inicial por consola.
+        System.out.println("--- Programa que solicita tres números y retorna cual de los tres es el mayor. ---");
         
+        // Solicita tres números al usuario.
+        System.out.print("\nIngrese el primer número: ");
+        Double numeroUno = ENTRADA().nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        Double numeroDos = ENTRADA().nextDouble();
+
+        System.out.print("Ingrese el tercer número: ");
+        Double numeroTres = ENTRADA().nextDouble();
+
+        // Condiciones para definir cual numero es mayor.
+        if (numeroUno > numeroDos & numeroUno > numeroTres){
+            System.out.printf("\n---> El Número %1$s es el mayor de los tres numeros ingresados.",numeroUno);
+        }else if(numeroDos > numeroUno & numeroDos > numeroTres){
+            System.out.printf("\n---> El Número %1$s es el mayor de los tres numeros ingresados.",numeroDos);
+        }else if(numeroTres > numeroUno & numeroTres > numeroDos){
+            System.out.printf("\n---> El Número %1$s es el mayor de los tres numeros ingresados.",numeroTres);
+        }
     }
 
     public static void main(String[] args) {
-        
+        numeroMayor();
     }
-
 }
