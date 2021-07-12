ecf715d0e8397da932968e4ec28b3700c174a99d
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 02:33:29 -0500

    Ejercicio_20 Updated

--- Ejercicios_U01/src/Ejercicio_20.java
+++ Ejercicios_U01/src/Ejercicio_20.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Ejercicio_20 {
    
@@ -8,12 +9,33 @@ public class Ejercicio_20 {
         calcule la longitud de la hipotenusa según el teorema de Pitágoras.
     */
 
-    public void name() {
-        
+    public static final Scanner ENTRADA() {
+        // Objeto Scanner llamado Entrada.
+        Scanner entrada = new Scanner(System.in);
+        return entrada;
+    }
+
+    public static int hipotenusa() {
+        // Solicita las longitudes de los catetos
+        System.out.print("Ingrese la longitud del primer cateto: ");
+        Double catetoA = ENTRADA().nextDouble();
+        System.out.print("Ingrese la longitud del segundo cateto: ");
+        Double catetoB = ENTRADA().nextDouble();
+
+        // Formula para calcular la hipotenusa
+        double hipotenusa = Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
+
+        // Conversión de los valores a integer.
+        int hipotenusaInt = (int) hipotenusa; 
+        return hipotenusaInt;
+    }
+
+    public static void mensajeFinal() {
+        // Mensaje final de la operación por consola.
+        System.out.printf("\n---> El valor redondeado de la hipotenusa es de: %1$s.", hipotenusa());        
     }
 
     public static void main(String[] args) {
-        
+        mensajeFinal();
     }
-
 }
