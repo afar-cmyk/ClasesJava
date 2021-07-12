3a05899c5221fbadaa1640614c74d431b95147d6
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-12 06:11:58 -0500

    Ejercicio_22 Updated

--- Ejercicios_U01/src/Ejercicio_22.java
+++ Ejercicios_U01/src/Ejercicio_22.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Ejercicio_22 {
 
@@ -7,12 +9,34 @@ public class Ejercicio_22 {
     22. Programa que lea un carácter por teclado y compruebe si es una letra mayúscula.
     */
 
-    public void name() {
-        
+    public static final Scanner ENTRADA() {
+        Scanner entrada = new Scanner(System.in);
+        return entrada;
+    }
+
+    public static void comprobarMayusculas() {
+        // Solicita una letra por consola.
+        System.out.println("Ingrese una letra: ");
+        String letra = ENTRADA().nextLine();
+
+        // Convierte la letra ingresada a char.
+        char letraChar = letra.charAt(0);
+
+        // Arreglo con todas las letras del abecedario en mayusculas
+        char[] listaLetras = new char[26];
+        for (int i = 0; i < 26; i++){
+            listaLetras[i] = (char) (65 + i);
+        }
+
+        // Condicional para definir si la letra ingresada es mayusculas o minusculas.
+        if (Arrays.binarySearch(listaLetras, letraChar) >= 0) {
+            System.out.println("El caracter ingresado esta en mayusculas.");
+        }else{
+            System.out.println("El caracter ingresado esta en minusculas o es un simbolo.");
+        }
     }
 
     public static void main(String[] args) {
-        
+        comprobarMayusculas();
     }
- 
 }
