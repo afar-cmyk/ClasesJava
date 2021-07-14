c31f9f0f5ffe7241fb684d4c1773814507269d7b
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-14 11:01:56 -0500

    Ejercicio_27 Updated

--- Ejercicios_U01/src/Ejercicio_27.java
+++ Ejercicios_U01/src/Ejercicio_27.java
@@ -9,7 +9,8 @@ public class Ejercicio_27 {
     */
 
     public static void condicionalFor() {
-
+        // Estructura For.
+        System.out.println("\n--- Números del 1 al 100 que no sean múltiplos de 3 con estructura 'For' ---\n");
         for (int i = 0; i < 100; i++){
 
             if (! (i % 3 == 0) ){
@@ -21,19 +22,33 @@ public class Ejercicio_27 {
     }
 
     public static void estructuraWhile() {
-
+        // Estructura While.
+        System.out.println("\n--- Números del 1 al 100 que no sean múltiplos de 3 con estructura 'While' ---\n");
         int i = 0;
         while(i < 101){
-            System.out.println((! (i % 3 == 0) ));
-            i  += 1; 
+
+            i  += 1;
+            if (! (i % 3 == 0) ){
+                System.out.println(i);
+            }
         }
-        
+    }
+
+    public static void estructuraDoWhile() {
+        // Estructura Do While.
+        System.out.println("\n--- Números del 1 al 100 que no sean múltiplos de 3 con estructura 'Do While' ---\n");
+        int i = 0;
+        do{
+            i  += 1;
+            if (! (i % 3 == 0) ){
+                System.out.println(i);
+            }
+        }while(i < 100);
     }
 
     public static void main(String[] args) {
-
+        condicionalFor();
         estructuraWhile();
-
+        estructuraDoWhile();
     }
-
 }
