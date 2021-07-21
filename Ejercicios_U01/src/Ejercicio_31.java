fc3bd778af5ff22a4663668cbf1e41417712a8aa
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-21 13:30:21 -0500

    Commit Ejercicio_31

---
+++ Ejercicios_U01/src/Ejercicio_31.java
@@ -0,0 +1,26 @@
+
+
+public class Ejercicio_31 {
+    
+    // Solo se puede utilizar la función main() solo para hacer el llamado a las funciones que se quieren probar.
+
+    /*
+    30. Programa que muestre en líneas separadas lo siguiente:
+    ZYWXVUTSRQPONMLKJIHGFEDCBA
+    YWXVUTSRQPONMLKJIHGFEDCBA
+    WXVUTSRQPONMLKJIHGFEDCBA
+    ....
+    DCBA
+    CBA
+    BA
+    A.
+    */
+
+    public static void name() {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
