8621bb8deb117c9d3b9a1ad5ec897d9b01fdb14a
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-07-24 15:03:23 -0500

    Ejercicio_32 Updated

--- Ejercicios_U01/src/Ejercicio_32.java
+++ Ejercicios_U01/src/Ejercicio_32.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejercicio_32 {
 
     //Solo se puede utilizar la función main() solo para hacer el llamado a las funciones que se quieren probar.
@@ -8,29 +11,37 @@ public class Ejercicio_32 {
 
     public static void contadorE() {
 
-        // Arreglo de enteros con los números del 1 al 9.
-        int[] prueba = {0,1,2,3,4,5,6,7,8,9};
-        // Variable que contiene la palabra E, para reemplazar el numero 3.
-        String palabra = "E";
+        //Colección de objetos de tipo Lista.
+        List<Object> lista = new ArrayList<>();
 
-        // Ciclo que recorerrá cada indice del arreglo de enteros
-        for (int uno = 0; uno <= 9; uno++) { 
-            for (int dos = 0; dos <= 9; dos++) {
-                for (int tres = 0; tres <= 9; tres++) {
-                    for (int cuatro = 0; cuatro <= 9; cuatro++) {
-                        for (int cinco = 0; cinco <= 9; cinco++) {
+        //Agrega a la lista los números del 1 al 9.
+        lista.add(0);
+        lista.add(1);
+        lista.add(2);
+        lista.add("E"); // En vez del 3 se pone un String con la letra "E".
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
 
-                            // Cuando el iterador sea igual a tres imprimirá la variable que contene la E.
-                             System.out.print(prueba[uno] == 3 ? palabra : prueba[uno]);
-                             System.out.print(prueba[dos] == 3 ? palabra : prueba[dos]);
-                             System.out.print(prueba[tres] == 3 ? palabra : prueba[tres]);
-                             System.out.print(prueba[cuatro] == 3 ? palabra : prueba[cuatro]);
-                             System.out.println(prueba[cinco] == 3 ? palabra : prueba[cinco]);                                           
+        // Ciclo que recorerrá cada indice de la lista hasta llegar a 99999.
+        for(Object uno : lista){
+            for (Object dos : lista) {
+                for (Object tres : lista) {
+                    for (Object cuatro : lista) {
+                        for (Object cinco : lista) {
+                            System.out.print(uno);
+                            System.out.print(dos);
+                            System.out.print(tres);
+                            System.out.print(cuatro);
+                            System.out.println(cinco);
                         }
                     }
                 }
             }
-        }
+        }   
     }
 
     public static void main(String[] args) {
